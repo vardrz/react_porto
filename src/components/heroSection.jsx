2a39d308b44f835cd63fe5c3ab0831ded16f6fb6
vardrz <fatkhurrozakf@gmail.com>
2024-04-01 13:34:47 +0700

    feat: add typing animation at home

--- src/components/heroSection.jsx
+++ src/components/heroSection.jsx
@@ -1,18 +1,39 @@
+import { TypeAnimation } from 'react-type-animation';
+
 export default function HeroSection() {
+    function typing(thisClass){
+        return (
+            <TypeAnimation
+                sequence={[
+                1000,
+                'I develop Web App💻',
+                2000,
+                'I develop Mobile App📱',
+                2000,
+                'I develop IoT Device💡',
+                1000,
+                ]}
+                speed={50}
+                className={thisClass}
+                repeat={Infinity}
+            />
+        )
+    }
+
     return (
         <>
             {/* Desktop */}
-            <div className="hidden w-full h-screen px-10 md:flex flex-col justify-center">
+            <div className="hidden w-full h-screen px-10 md:flex flex-col justify-center pt-10">
                 <span className="hero-text text-8xl text-light">Farid Fatkhurrozak</span>
-                <span className="hero-text text-5xl lg:text-6xl text-primary">Software Developer👨‍💻</span>
+                {typing("hero-text text-5xl lg:text-6xl mt-2 text-primary")}
                 <span className="mt-10 text-3xl text-light">❝ No Hugging, Only Debugging ❞ 🤸‍♂️</span>
             </div>
 
             {/* Mobile */}
-            <div className="w-full h-screen px-10 flex flex-col justify-center md:hidden">
+            <div className="w-full h-screen px-8 flex flex-col justify-center md:hidden">
                 <span className="hero-text text-7xl sm:text-8xl text-light">Farid</span>
                 <span className="hero-text text-4xl sm:text-6xl text-light">Fatkhurrozak</span>
-                <span className="mt-5 hero-text text-xl sm:text-2xl text-primary">Software Developer👨‍💻</span>
+                {typing("mt-5 hero-text text-xl sm:text-2xl text-primary")}
                 <span className="mt-10 text-base text-light">❝ No Hugging, Only Debugging ❞ 🤸‍♂️</span>
             </div>
         </>
