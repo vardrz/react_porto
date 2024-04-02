65d3b99aa62f36c688d5a4160f87a40d6df34135
vardrz <fatkhurrozakf@gmail.com>
2024-04-02 07:09:17 +0700

    feat: add skeleton on projects load

--- src/views/projects.jsx
+++ src/views/projects.jsx
@@ -61,7 +61,21 @@ function ProjectSection(){
                         </div>
                     ))
                     : (
-                        <div className="text-3xl text-primary w-full inline-flex justify-center">Load Projects ..</div>
+                        <div className="w-full flex flex-col-reverse mb-16 md:flex-row md:items-center">
+                            <div className="md:w-2/4">
+                                <div className="bg-dark2 animate-pulse rounded-md w-3/5 h-7"></div>
+                                <div className="bg-dark2 animate-pulse rounded-md mt-7 full h-5"></div>
+                                <div className="bg-dark2 animate-pulse rounded-md mt-4 full h-5"></div>
+                                <div className="bg-dark2 animate-pulse rounded-md mt-4 full h-5"></div>
+                            </div>
+                            <div className="md:w-2/4 mb-10 md:mb-0 md:px-10">
+                                <div className="bg-dark2 animate-pulse rounded-md w-full p-12 inline-flex justify-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="text-dark w-16 md:w-24">
+                                        <path fillRule="evenodd" d="M1.5 6a2.25 2.25 0 0 1 2.25-2.25h16.5A2.25 2.25 0 0 1 22.5 6v12a2.25 2.25 0 0 1-2.25 2.25H3.75A2.25 2.25 0 0 1 1.5 18V6ZM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0 0 21 18v-1.94l-2.69-2.689a1.5 1.5 0 0 0-2.12 0l-.88.879.97.97a.75.75 0 1 1-1.06 1.06l-5.16-5.159a1.5 1.5 0 0 0-2.12 0L3 16.061Zm10.125-7.81a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Z" clipRule="evenodd" />
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
                     )
             }
         </div>
