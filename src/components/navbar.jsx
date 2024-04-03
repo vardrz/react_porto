e84528ba521672aab9aeec37343da34009ed7c51
vardrz <fatkhurrozakf@gmail.com>
2024-04-03 21:22:27 +0700

    feat: implement swiperjs on project detail

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,23 @@
+/* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
 import DrawerMenu from "./drawerMenu";
 
-export default function Navbar(){
+export default function Navbar(props){
     const hash = window.location.hash;
     const avatar = "me-circle.png";
-
+    
     function HomeLink(){
+        if(props.from == 'projects'){
+            return (
+                <Link to="/projects" className="flex flex-row items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+                        <path fillRule="evenodd" d="M7.72 12.53a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 1 1 1.06 1.06L9.31 12l6.97 6.97a.75.75 0 1 1-1.06 1.06l-7.5-7.5Z" clipRule="evenodd" />
+                    </svg>
+                    <span className="ml-1">Projects</span>
+                </Link>
+            )
+        }
+
         if(hash == '' || hash == '#/'){
             return (
                 <div className="w-10 sm:w-14 rounded-full p-[0.5px] border-2 border-primary">
--- src/views/projectsDetail.jsx
+++ src/views/projectsDetail.jsx
@@ -1,5 +1,12 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 import { Navigate, useLocation } from "react-router-dom";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from 'swiper/modules';
+
+import Navbar from "../components/navbar";
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 export default function ProjectDetail(){
     const data = useLocation();
@@ -8,9 +15,47 @@ export default function ProjectDetail(){
         ? <Navigate to="/notfound" state={{ from: 'projects' }} />
         : (
             <>
-                <div className="text-lg text-light">
-                    {data.state.title}
+                <Navbar from="projects"/>
+                <ProjectImages data={data.state}/>
+                <div className="w-full px-10 md:px-28 py-16 text-xl md:text-3xl text-light whitespace-pre-wrap">
+                    {data.state.fullDesc}
                 </div>
             </>
         )
+}
+
+function ProjectImages(data){
+    const images = data.data.images.split(',')
+    const classWidthSlider = data.data.createBy != 'Flutter' ? 'md:w-3/4 rounded-md' : 'h-96 md:h-screen rounded-md'
+
+    return (
+        <div className="w-full px-10 pt-32">
+            <div className='w-full inline-flex justify-center'>
+                <img src={"/projects/"+images[0]} className='w-full md:w-1/2 rounded-lg mb-5' />
+            </div>
+            <Swiper
+                slidesPerView={1}
+                spaceBetween={30}
+                loop={true}
+                pagination={{
+                clickable: true,
+                }}
+                navigation={true}
+                modules={[Pagination, Navigation]}
+                className="mySwiper"
+            >
+                {
+                    images.map((item, index) => (
+                        index != 0 ?
+                        <SwiperSlide key={index}>
+                            <div className="w-full inline-flex justify-center">
+                                <img src={"/projects/" + item} className={classWidthSlider} />
+                            </div>
+                        </SwiperSlide>
+                        : ''
+                    ))
+                }
+            </Swiper>
+        </div>
+    )
 }
